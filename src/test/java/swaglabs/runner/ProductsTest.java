a0aff253d6c8215f3b71b458ed26ce763d45aa1b
Yuliya48 <kolyabina8@gmail.com>
2023-09-25 18:17:52 -0400

    add testProductAddedRemovedToCart()

--- src/test/java/swaglabs/runner/ProductsTest.java
+++ src/test/java/swaglabs/runner/ProductsTest.java
@@ -3,17 +3,22 @@ package swaglabs.runner;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import swaglabs.pages.LoginPage;
+import swaglabs.pages.ProductsPage;
 
 public class ProductsTest extends BaseTest {
 
     @Test
-    public void testLogOut(int numUser) {
-        String actualLogoText = new LoginPage(getDriver()).validLogin(numUser)
-                .menuClick()
-                .logOutClick()
-                .loginLogo.getText();
-        String expectedLogoText = "Swag Labs";
+    public void testProductAddedRemovedToCart() throws InterruptedException {
+        ProductsPage productPage = new LoginPage(getDriver()).validLogin();
+        productPage
+                .addAllItemsToCart();
 
-        Assert.assertEquals(actualLogoText, expectedLogoText);
+        String afterAddingCartText = productPage.getTextCartLink();
+
+        productPage
+                .removeAllItemsToCart();
+        String finalCartText = productPage.getTextCartLink();
+        Assert.assertNotEquals(afterAddingCartText, finalCartText,
+                "Cart should not be empty after adding items.");
     }
 }
